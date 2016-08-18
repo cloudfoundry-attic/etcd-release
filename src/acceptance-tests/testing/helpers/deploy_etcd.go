5d7729323b96c2502d53d2840b16aefbf83afc55
Adrian Zankich <azankich@pivotal.io>
2016-08-17 18:00:45 -0700

    Add initial support for running EATS in parallel
    [#127956539]

--- src/acceptance-tests/testing/helpers/deploy_etcd.go
+++ src/acceptance-tests/testing/helpers/deploy_etcd.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 
+	ginkgoConfig "github.com/onsi/ginkgo/config"
 	"github.com/pivotal-cf-experimental/bosh-test/bosh"
+	"github.com/pivotal-cf-experimental/destiny/core"
 	"github.com/pivotal-cf-experimental/destiny/etcd"
 	"github.com/pivotal-cf-experimental/destiny/iaas"
 )
@@ -51,27 +53,42 @@ func buildManifestInputs(config Config, client bosh.Client) (manifestConfig etcd
 
 	switch info.CPI {
 	case "aws_cpi":
-		iaasConfig = iaas.AWSConfig{
+		awsConfig := iaas.AWSConfig{
 			AccessKeyID:           config.AWS.AccessKeyID,
 			SecretAccessKey:       config.AWS.SecretAccessKey,
 			DefaultKeyName:        config.AWS.DefaultKeyName,
 			DefaultSecurityGroups: config.AWS.DefaultSecurityGroups,
 			Region:                config.AWS.Region,
-			Subnet:                config.AWS.Subnet,
 			RegistryHost:          config.Registry.Host,
 			RegistryPassword:      config.Registry.Password,
 			RegistryPort:          config.Registry.Port,
 			RegistryUsername:      config.Registry.Username,
 		}
-		if config.AWS.Subnet != "" {
-			manifestConfig.IPRange = "10.0.16.0/24"
-		} else {
+		if config.AWS.Subnet == "" {
 			err = errors.New("AWSSubnet is required for AWS IAAS deployment")
 			return
 		}
+		var cidrBlock string
+		cidrPool := core.NewCIDRPool("10.0.16.0", 24, 27)
+		cidrBlock, err = cidrPool.Get(ginkgoConfig.GinkgoConfig.ParallelNode)
+		if err != nil {
+			return
+		}
+
+		manifestConfig.IPRange = cidrBlock
+		awsConfig.Subnets = []iaas.AWSConfigSubnet{{ID: config.AWS.Subnet, Range: cidrBlock, AZ: "us-east-1a"}}
+
+		iaasConfig = awsConfig
 	case "warden_cpi":
 		iaasConfig = iaas.NewWardenConfig()
-		manifestConfig.IPRange = "10.244.16.0/24"
+
+		var cidrBlock string
+		cidrPool := core.NewCIDRPool("10.244.16.0", 24, 27)
+		cidrBlock, err = cidrPool.Get(ginkgoConfig.GinkgoConfig.ParallelNode)
+		if err != nil {
+			return
+		}
+		manifestConfig.IPRange = cidrBlock
 	default:
 		err = errors.New("unknown infrastructure type")
 	}
@@ -96,18 +113,28 @@ func NewEtcdWithInstanceCount(count int, client bosh.Client, config Config, enab
 	}
 
 	if enableSSL {
-		manifest = etcd.NewTLSManifest(manifestConfig, iaasConfig)
+		manifest, err = etcd.NewTLSManifest(manifestConfig, iaasConfig)
+		if err != nil {
+			return
+		}
 	} else {
-		manifest = etcd.NewManifest(manifestConfig, iaasConfig)
+		manifest, err = etcd.NewManifest(manifestConfig, iaasConfig)
+		if err != nil {
+			return
+		}
 	}
 
-	manifest = SetEtcdInstanceCount(3, manifest)
+	manifest, err = SetEtcdInstanceCount(3, manifest)
 
 	return
 }
 
 func SetEtcdInstanceCount(count int, manifest etcd.Manifest) etcd.Manifest {
-	manifest.Jobs[1] = etcd.SetJobInstanceCount(manifest.Jobs[1], manifest.Networks[0], count, 0)
+	var err error
+	manifest.Jobs[1], err = etcd.SetJobInstanceCount(manifest.Jobs[1], manifest.Networks[0], count, 0)
+	if err != nil {
+		return manifest, err
+	}
 	manifest.Properties = etcd.SetEtcdProperties(manifest.Jobs[1], manifest.Properties)
 
 	return manifest
@@ -119,7 +146,10 @@ func SetTestConsumerInstanceCount(count int, manifest etcd.Manifest) (etcd.Manif
 		return manifest, err
 	}
 
-	manifest.Jobs[jobIndex] = etcd.SetJobInstanceCount(manifest.Jobs[jobIndex], manifest.Networks[0], count, 8)
+	manifest.Jobs[jobIndex], err = etcd.SetJobInstanceCount(manifest.Jobs[jobIndex], manifest.Networks[0], count, 8)
+	if err != nil {
+		return manifest, err
+	}
 
 	return manifest, nil
 }
